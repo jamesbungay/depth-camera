1211a7cc384299c83d1303ae3fdefa914fa55236
James Bungay <jtb.github@gmail.com>
2021-10-25 16:19:09 +0100

    Initial Commit

---
+++ TremorCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TremorCamera
+//
+//  Created by James Bungay on 25/10/2021.
+//  Copyright © 2021 James Bungay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
